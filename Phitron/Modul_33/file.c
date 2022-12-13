12da6da93ef51d35877961d626ba7707f5488cca
MSI75 <sisaif56@gmail.com>
2022-12-13 17:13:58 +0600

    prob 2

--- Phitron/Modul_33/file.c
+++ Phitron/Modul_33/file.c
@@ -19,6 +19,11 @@ int main(){
     FILE *inputFile;
     inputFile = fopen("input.txt","r");
 
+    if(inputFile==NULL){
+        printf("No File Found!\n");
+        return 0;
+    }
+
     FILE *outputFile;
     outputFile = fopen("output.txt","w");
 
