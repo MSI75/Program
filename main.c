fc7eabf1aac5aafd947f1b71dbb9641b011d91d9
MSI75 <sisaif56@gmail.com>
2022-12-14 18:00:42 +0600

    add

--- main.c
+++ main.c
@@ -1,21 +1,42 @@
+// #include<stdio.h>
+
+// int main(){
+//     int n, i, x;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i = 1; i<n; i++){
+//         scanf("%d", &x);
+//         arr[x]=1;
+//     }
+//     for(int i= 1; i<=n; i++){
+//         if(arr[i]==1){
+//             continue;
+//         }
+//         printf("%d\n",i);
+//     }
+//     return 0;
+// }
+
 #include<stdio.h>
 
 int main(){
-    int n;
-    scanf("%d", &n);
-    int arr[n];
-    for(int i = 0; i<n;i++){
-       scanf("%d",&arr[i]);
-    }
-
-    for(int i = 0; i<n; i++){
-        if(arr[i]==i){
-            printf(" ");
-        }else{
-            printf("%d", i);
+    int n, m;
+    scanf("%d",&n);
+ 
+    for(int i = 1; i<=n; i++){
+        scanf("%d", &m);
+        int arr[m];
+        for(int j = 1; j<=m; j++){
+            scanf("%d",&arr[j]);
+            arr[j]=1;
         }
-    }
-    
 
-    
+        for(int i = 1; i<=m; i++){
+        if(arr[i]==1){
+            continue;
+        }
+        printf("%d\n",i);
+        break;
+       }
+    }
 }
